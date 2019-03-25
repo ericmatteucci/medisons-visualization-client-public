a1e3c07a0b76b1f38491215f21987815876af183
Eric Matteucci <eric.matteucci@ucalgary.ca>
2019-03-25 00:16:55 -0600

    deleted an unused constant

--- src/constants/ValueConstants.js
+++ src/constants/ValueConstants.js
@@ -4,8 +4,6 @@ export const ONE_SECOND_MS = 1000;
 export const FIVE_SECONDS_MS = ONE_SECOND_MS * 5;
 export const ONE_MINUTE_MS = ONE_SECOND_MS * 60;
 
-export const CHART_WINDOW_SIZE = ONE_MINUTE_MS;
-
 export const SCORE_LOAD_INTERVAL = FIVE_SECONDS_MS;
 
 export const SignalNames = {
